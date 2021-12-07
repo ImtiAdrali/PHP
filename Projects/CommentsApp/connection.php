d426ede6df128abc261064aff7eee516d83b8fdd
ImtiAdrali <92763832+ImtiAdrali@users.noreply.github.com>
2021-12-07 11:17:49 -0500

    Delete connection.php

--- Projects/CommentsApp/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("Host", "helios.vse.gmu.edu");
-define("User", "iahmad8");
-define("Password", "voopte");
-define("Databse", "iahmad8");
-
-$DBConnection = mysqli_connect(Host, User, Password, Databse) or 
-        die('connection is not successful Error: '. mysqli_connect_error());
